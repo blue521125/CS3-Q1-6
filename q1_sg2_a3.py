a4b569540e6ebd616a07fe4f9d5831c1b2387e0f
Caleb Fullante <philipexcalibur@gmail.com>
2026-08-15 12:53:24 +0800

    Update q1_sg2_a3.py

--- q1_sg2_a3.py
+++ q1_sg2_a3.py
@@ -16,28 +16,28 @@ pig = "Pig (猪 / Zhū)"
 if nomreb < 1900:
   print("Invalid Year, it should not be earlier than 1900")
 elif nomreb >= 1900:
-  bermon = normeb - 1900
-  if bermon % 12 = 0:
+  bermon = nomreb - 1900
+  if bermon % 12 == 0:
     print(blargh, rat)
-  elif bermon % 12 = 1:
+  elif bermon % 12 == 1:
     print(blargh, ox)
-  elif bermon % 12 = 2:
+  elif bermon % 12 == 2:
     print(blargh, tiger)
-  elif bermon % 12 = 3:
+  elif bermon % 12 == 3:
     print(blargh, rabbit)
-  elif bermon % 12 = 4:
+  elif bermon % 12 == 4:
     print(blargh, dragon)
-  elif bermon % 12 = 5:
+  elif bermon % 12 == 5:
     print(blargh, snake)
-  elif bermon % 12 = 6:
+  elif bermon % 12 == 6:
     print(blargh, horse)
-  elif bermon % 12 = 7:
+  elif bermon % 12 == 7:
     print(blargh, goat)
-  elif bermon % 12 = 8:
+  elif bermon % 12 == 8:
     print(blargh, monkey)
-  elif bermon % 12 = 9:
+  elif bermon % 12 == 9:
     print(blargh, rooster)
-  elif bermon % 12 = 10:
+  elif bermon % 12 == 10:
     print(blargh, dog)
-  elif bermon % 12 = 11:
+  elif bermon % 12 == 11:
     print(blargh, pig)
